e698d68e7a524c5735df43b413d059df028dbb12
Natan Król <natankrol5@gmail.com>
2022-10-24 23:57:02 +0200

    contact page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { initial } from "./initial";
 import { menu } from "./menu";
+import { contact } from "./contact";
 const content = document.getElementById('content');
 
 
@@ -7,15 +8,33 @@ window.addEventListener('load', initial());
 const restaurantPhoto = document.querySelector('.restaurant-photo');
 
 menu()
+contact()
 
 
+// menu
 let menuBtn = document.querySelector('.menu-btn');
-let menuBoard = document.querySelector('.menu-board')
-let mainSection = document.querySelector('.main-section')
+let menuBoard = document.querySelector('.menu-board');
+// contact
+let contactBtn = document.querySelector('.contact-btn');
+let contactBoard = document.querySelector('.contact-board');
+
+let mainSection = document.querySelector('.main-section');
+
+
 menuBtn.addEventListener('click', function(){
     restaurantPhoto.classList.add('restaurant-photo-hidden');
-    mainSection.appendChild(menuBoard)
-})
+    mainSection.appendChild(menuBoard);
+    menuBoard.classList.remove('board-hidden');
+    contactBoard.classList.add('board-hidden');
+});
+
+contactBtn.addEventListener('click', function(){
+    restaurantPhoto.classList.add('restaurant-photo-hidden');
+    mainSection.appendChild(contactBoard);
+    contactBoard.classList.remove('board-hidden');
+    menuBoard.classList.add('board-hidden');
+});
+
 
 
 
--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,7 @@
-// import { ContextReplacementPlugin } from "webpack";
-
 export let menu =() =>{
-
     let menuBoard = document.createElement('div');
     menuBoard.classList.add('menu-board');
+    menuBoard.classList.add('board-hidden');
     menuBoard.innerHTML = "<h1>Our Menu</h1><ul class='menu-list'><li>Spaghetti</li><li>Fried Chicken</li><li>Lasagne</li><li>PulledPork</li><li>Cheeseburger</li><li>Fries</li></ul>"
     content.appendChild(menuBoard);
 }
